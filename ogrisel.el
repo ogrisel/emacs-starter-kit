21ea5ac7cc261b2eecf9d358e8e184c2cc6e191b
Olivier Grisel <olivier.grisel@ensta.org>
2010-09-05 13:58:58 +0200

    cosmit

--- ogrisel.el
+++ ogrisel.el
@@ -1,14 +1,15 @@
-;(load-file "~/.emacs.d/ogrisel/color-theme-sunburst.el")
 (load-file (concat user-specific-dir "/color-theme-sunburst.el"))
 (color-theme-tm)
 
+
+;; Smaller font size to allow for 2x80 chars vertical split on 13 inches screen
 (defun fontify-frame (frame)
   (set-frame-parameter frame 'font "Monospace-8"))
-
 ;; Fontify current frame
 (fontify-frame nil)
 ;; Fontify any future frames
 (push 'fontify-frame after-make-frame-functions)
 
+;; Ctrl-F5 to display line numbers
 (autoload 'linum-mode "linum" "toggle line numbers on/off" t)
 (global-set-key (kbd "C-<f5>") 'linum-mode)
